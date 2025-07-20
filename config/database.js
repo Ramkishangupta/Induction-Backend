155d49f05e03c76f3453b85c4848825cc349de7a
Ramkishan Gupta <145246814+Ramkishangupta@users.noreply.github.com>
2025-07-20 20:15:42 +0530

    updated database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = ()=>{
-    mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  maxPoolSize: 50,
-})
+    mongoose.connect(process.env.mongodbURL,{useNewUrlParser: true,useUnifiedTopology: true, maxPoolSize: 50}).then((data)=>{
+        console.log(`Mongodb connected with server`);
+    })
 }
 
 module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -2,13 +2,9 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 const app = require("./app");
 
-// Load config
 dotenv.config();
-
-// Connect to MongoDB
 connectDatabase();
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on:${PORT}`);

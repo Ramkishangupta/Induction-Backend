ac4be71c9470a2da10e609516fd9abc11fd1e813
Ramkishan Gupta <145246814+Ramkishangupta@users.noreply.github.com>
2025-07-22 21:18:49 +0530

    allowing all origins

--- app.js
+++ app.js
@@ -3,20 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = [
-  'https://varaha.itch.io/feinduction',
-  'https://leaderboard-2025.vercel.app'
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
+app.use(cors());
 app.use(express.json());
 
 

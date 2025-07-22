a1214516e0dc2497a4ed2d6e27c0eedcf560e320
Ramkishan Gupta <145246814+Ramkishangupta@users.noreply.github.com>
2025-07-22 21:13:31 +0530

    chal jaa

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const cors = require('cors');
 
 const app = express();
 
+const allowedOrigins = [
+  'https://varaha.itch.io/feinduction',
+  'https://leaderboard-2025.vercel.app'
+];
+
 const corsOptions = {
-  origin: [
-
-  ]
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 };
-
-app.use(cors({
-  origin: ['https://varaha.itch.io/feinduction','https://leaderboard-2025.vercel.app']
-}));
-
 app.use(express.json());
 
 

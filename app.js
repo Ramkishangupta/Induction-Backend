516c9ba0264ff03c6154f42e08c38d7b97da368f
Ramkishan Gupta <145246814+Ramkishangupta@users.noreply.github.com>
2025-07-22 20:45:01 +0530

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const cors = require('cors'); // 🔹 Import CORS
 
 const app = express();
 
+app.use(cors()); // 🔹 Enable CORS for all origins
 app.use(express.json());
 
-//route import
+// 🔹 Route import
 const playerRoutes = require("./routes/playerRoutes");
 
 app.use("/api/v1", playerRoutes);
-
 
 module.exports = app;
